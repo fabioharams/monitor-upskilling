c04619e61b1f8129a063d6409bceca166e9fc143
Fabricio Catae <fcatae@microsoft.com>
2018-02-07 11:47:31 -0200

    add project Cmd

---
+++ src/Cmd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
